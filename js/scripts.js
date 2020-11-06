b5ed3f9e121c792b625b161d3ce9d91ccb9d5697
Ethan Roldan <erolda266@west-mec.org>
2020-11-05 18:02:20 -0700

    worked on home page

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,16 @@ function darkDiv(){
     var element = document.body;
     if(lights){
         document.getElementById("darkDiv").style.backgroundColor = "#333333";
+        document.getElementById("darkDiv2").style.backgroundColor = "#333333";
         document.getElementById("header").style.backgroundColor = "#222222";
         document.getElementById("link").style.color = "white";
+        document.getElementById("link2").style.color = "white";
     }else{
-        document.getElementById("darkDiv").style.backgroundColor = "#777777";
+        document.getElementById("darkDiv").style.backgroundColor = "#888888";
+        document.getElementById("darkDiv2").style.backgroundColor = "#888888";
         document.getElementById("header").style.backgroundColor = "yellow";
         document.getElementById("link").style.color = "black";
+        document.getElementById("link2").style.color = "black";
     }
 }
 
