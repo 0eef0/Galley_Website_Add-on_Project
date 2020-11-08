0e44baeff9be8cd25b14538b0d7babdd66572ae8
TheCapn-MEC <erolda266@west-mec.org>
2020-11-08 00:20:46 -0700

    Fixed switch issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
+//this is just a function that runs other functions
 function changeEverything(){
     darkToggle();
     darkDiv();
     changeImage();
 }
 
+//this changes text color
 function darkToggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
 
+//darkDiv and changeImage change elements like divs and the switch betwee ndark and light mode
 var lights = true;
 function darkDiv(){
     var element = document.body;
@@ -17,31 +20,39 @@ function darkDiv(){
         for (var i = 0; i < x.length; i++) {
         x[i].style.backgroundColor = "rgb(37, 39, 38, 0.9)";
         }
-        document.getElementById("character").style.border = "white 2px solid";
-        //document.getElementsByClassName("darkDiv").style.backgroundColor = "rgb(37, 39, 38, 0.9)";
     }else{
         var x = document.getElementsByClassName("darkDiv");
         for (var i = 0; i < x.length; i++) {
         x[i].style.backgroundColor = "rgb(255, 255, 255, 0.9)";
         }
-        document.getElementById("character").style.border = "black 2px solid";
-        //document.getElementsByClassName("darkDiv").style.backgroundColor = "rgb(255, 255, 255, 0.9)";
     }
 }
 
 function changeImage()
 {
     if(lights){
-        var img = document.getElementById("image");
-        img.src="images/switchOFF.png";
+        var img = document.getElementsByClassName("image");
+        for(var i = 0; i < img.length; i++){
+            img[i].src = "images/switchOFF.png";
+        }
         lights = false;
     }else{
-        var img = document.getElementById("image");
-        img.src="images/switchON.png";
+        var img = document.getElementsByClassName("image");
+        for(var i = 0; i < img.length; i++){
+            img[i].src = "images/switchON.png";
+        }
         lights = true;
     }
 }
 
+//below is the scripting for the bio page
+function charBorder(){
+    if(!lights){
+        document.getElementById("character").style.border = "white 2px solid";
+    }else{
+        document.getElementById("character").style.border = "black 2px solid";
+    }
+}
 var phoenix = "Phoenix Wright is the protagonist. He is a new defense attorney who is determined to get the not guilty verdict for his client.";
 var mia = "Mia Fey is Phoenix's superior and the chief of Fey & Co. Law Offices. She is often looked up to as a really good defense attorney, and this shows in Phoenix's skills coming from her.";
 var maya = "Maya Fey is Mia's little sister. She is also a powerful spirit medium. Her skills as a medium are shown when she channels Mia to assist Phoenix in court. Unfortuneately, she tends to be accused of murder more than the average person.";
